0fee7ce18c89daa2e425f2464bef18abbb2da412
abdulkadir erbas <akadir.erbass@gmail.com>
2024-08-13 03:58:44 +0000

    opentelemetry integration was made

--- src/app.js
+++ src/app.js
@@ -1,7 +1,13 @@
 import express from "express";
 import userRoutes from "./routes/UserRoutes.js";
+import dotenv from "dotenv";
 
 const app = express();
+
+dotenv.config();
+const port = process.env.PORT;
+
+
 
 // Middleware to parse JSON bodies
 app.use(express.json());
@@ -9,6 +15,6 @@ app.use(express.json());
 // Use the user routes
 app.use('/api', userRoutes);
 
-app.listen(3000, () => {
-    console.log("Server running on port 3000");
+app.listen(port, () => {
+    console.log(`Server running on 10.150.238.233:${port}`);
 });
