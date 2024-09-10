dc2570255ca14d623e2c4efb8f236b0ba234bbc9
Kadir Erbas <akadir.erbass@gmail.com>
2024-09-10 12:10:35 +0000

    elk has been entegreted

--- src/routes/AccountRoutes.js
+++ src/routes/AccountRoutes.js
@@ -7,7 +7,7 @@ router.get('/', getAccounts);
 router.get('/:id', getAccountById);
 router.post('/', createAccount);
 router.put('/:id', updateAccount);
-router.patch('/:id', updateAccount); // Kısmi güncelleme için aynı controller'ı kullanabilirsiniz
+router.patch('/:id', updateAccount); 
 router.delete('/:id', deleteAccount);
 
 export default router;
